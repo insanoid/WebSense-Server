31fab4992e15469966f4847b1e5fc6839c3d889e
Karthikeya Udupa Kuppar Manjunath <kxu356@vm-kxu356.cs.bham.ac.uk>
2014-08-11 23:46:07 +0100

    Changes in app.js

--- app.js
+++ app.js
@@ -26,17 +26,16 @@ databaseconnection.connect(config.mongo.host, config.mongo.port, function(_dbCon
 
 
 // all environments
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 21129);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.json({limit: '500mb'}));
+app.use(express.urlencoded({limit: '500mb'}));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // development only
 if ('development' == app.get('env')) {
